9db9755e03ca086d53f65b4a745a525741528557
Neville JS <nevillejs24@gmail.com>
2025-02-13 00:45:06 +0530

    config change

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [glsl()],
+  server: {
+      host: '0.0.0.0',
+      port: 5173,    
+      strictPort: true,
+    },
 });
